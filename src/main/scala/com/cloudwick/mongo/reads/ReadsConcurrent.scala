5c63ff4a7fff316f8d2c0bf6c7fe797ed17d1558
ashrithr <ashrith@me.com>
2013-11-25 12:49:07 -0800

    Mongo - show time for all thread operations in random reads

--- src/main/scala/com/cloudwick/mongo/reads/ReadsConcurrent.scala
+++ src/main/scala/com/cloudwick/mongo/reads/ReadsConcurrent.scala
@@ -5,6 +5,7 @@ import com.cloudwick.mongo.dao.LogDAO
 import org.slf4j.LoggerFactory
 import java.util.concurrent.{Executors, ExecutorService}
 import java.util.concurrent.atomic.AtomicLong
+import com.cloudwick.generator.utils.Utils
 
 /**
  * Performs random reads using pool of threads
@@ -19,17 +20,20 @@ class ReadsConcurrent (totalDocuments: Int, events: Long, config: OptionsConfig,
   val threadPool: ExecutorService = Executors.newFixedThreadPool(config.threadPoolSize)
   val finalCounter:AtomicLong = new AtomicLong(0L)
   val queriesPerThread = events / config.threadCount
+  val utils = new Utils
 
   def run() = {
-    try {
-      (1 to config.threadCount).foreach { threadCount =>
-        logger.info(s"Initializing thread$threadCount")
-        threadPool.execute(new Reads(queriesPerThread, totalDocuments, finalCounter, config, mongo))
+    utils.time(s"reading $events") {
+      try {
+        (1 to config.threadCount).foreach { threadCount =>
+          logger.info(s"Initializing thread$threadCount")
+          threadPool.execute(new Reads(queriesPerThread, totalDocuments, finalCounter, config, mongo))
+        }
+      } finally {
+        threadPool.shutdown()
       }
-    } finally {
-      threadPool.shutdown()
+      while(!threadPool.isTerminated) {}
+      logger.info(s"Total read queries executed by ${config.threadCount} threads: " + finalCounter)
     }
-    while(!threadPool.isTerminated) {}
-    logger.info(s"Total read queries executed by ${config.threadCount} threads: " + finalCounter)
   }
 }
